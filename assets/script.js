a3fdc8e90edaaea0f68482f157ca743e270cb280
eurogb <rio1zamet@gmail.com>
2025-09-24 15:40:44 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -46,3 +46,12 @@ script.onload = () => {
   }
 };
 document.head.appendChild(script);
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menuToggle");
+  const links = document.getElementById("menuLinks");
+
+  toggle?.addEventListener("click", () => {
+    links.classList.toggle("show");
+  });
+});
+
